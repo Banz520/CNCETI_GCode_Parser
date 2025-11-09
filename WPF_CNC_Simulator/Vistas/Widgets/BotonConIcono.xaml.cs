a386e4fe53cb784f21548ad8bf027f08ca71b8f0
Banzi Alvarez <ianbanzialvarezf@gmail.com>
2025-11-08 18:00:03 -0600

    Implementacion de GUI

---
+++ WPF_CNC_Simulator/Vistas/Widgets/BotonConIcono.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CNC_Simulator.Vistas.Widgets
+{
+    /// <summary>
+    /// Lógica de interacción para BotonConIcono.xaml
+    /// </summary>
+    public partial class BotonConIcono : UserControl
+    {
+
+
+        public string Titulo
+        {
+            get { return (string)GetValue(TituloProperty); }
+            set { SetValue(TituloProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Titulo.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TituloProperty =
+            DependencyProperty.Register("Titulo", typeof(string), typeof(BotonConIcono), new PropertyMetadata("Texto Titulo"));
+
+
+
+        // Propiedad Icono (tipo ImageSource)
+        public static readonly DependencyProperty IconoProperty =
+            DependencyProperty.Register(
+                nameof(Icono),
+                typeof(ImageSource),
+                typeof(BotonConIcono),
+                new PropertyMetadata(null));
+
+        public ImageSource Icono
+        {
+            get => (ImageSource)GetValue(IconoProperty);
+            set => SetValue(IconoProperty, value);
+        }
+
+
+
+        public BotonConIcono()
+        {
+            InitializeComponent();
+        }
+    }
+}
